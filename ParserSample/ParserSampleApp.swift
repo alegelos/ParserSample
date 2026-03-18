73938031633097e989b9e20fa3e7e815a727e673
Alejandro Gelos <alegelos@gmail.com>
2026-03-18 13:36:07 +0100

    chore: initialize project

---
+++ ParserSample/ParserSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParserSampleApp.swift
+//  ParserSample
+//
+//  Created by Alejandro on 18/3/26.
+//
+
+import SwiftUI
+
+@main
+struct ParserSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ParserSampleTests/ParserSampleTests.swift
@@ -0,0 +1,16 @@
+//
+//  ParserSampleTests.swift
+//  ParserSampleTests
+//
+//  Created by Alejandro on 18/3/26.
+//
+
+import Testing
+
+struct ParserSampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
